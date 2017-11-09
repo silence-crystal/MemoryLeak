0476100fc6a1d9989a5026dbd74d4e515fb2303a
silence-crystal <529832605@qq.com>
2017-11-09 18:03:22 +0800

    试验

---
+++ app/src/main/java/com/example/administrator/neicunyouhua/CUtils.java
@@ -0,0 +1,22 @@
+package com.example.administrator.neicunyouhua;
+
+import android.content.Context;
+
+/**
+ * Created by cuizhize on 2017/10/25.
+ */
+
+public class CUtils {
+    private static CUtils instance;
+    private Context context;
+    private CUtils(Context context){
+        this.context = context;
+    }
+
+    public static CUtils getInstance(Context context){
+        if (instance == null){
+            instance = new CUtils(context);
+        }
+        return instance;
+    }
+}
